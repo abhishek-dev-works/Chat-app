13962818c8a84c256348a554cfe5236c1592cc1f
Abhishek <abhishek.works22@gmail.com>
2023-10-09 00:35:34 +0530

    Added meta tags

--- src/Layout/Messaging.tsx
+++ src/Layout/Messaging.tsx
@@ -11,7 +11,7 @@ const useStyles = createUseStyles({
   container: {
     width: "100%",
     backgroundColor: "#1F1F1F",
-    height: '100vh',
+    height: "100vh",
     "&::-webkit-scrollbar": {
       width: "0",
     },
@@ -33,19 +33,19 @@ const useStyles = createUseStyles({
   },
   chatsBox: {
     width: "100%",
-    height: '70%',
+    height: "70%",
     display: "flex",
     flexDirection: "column",
     padding: 10,
-    overflow: 'hidden',
-    overflowY: 'scroll'
+    overflow: "hidden",
+    overflowY: "scroll",
   },
   messageBox: {
     width: "98%",
     backgroundColor: "#F4F4F9",
     borderRadius: 40,
-    height: '20%',
-    margin: '10px auto'
+    height: "20%",
+    margin: "10px auto",
   },
 });
 
@@ -57,16 +57,48 @@ const Messaging = (props?: IMessagingPropTypes) => {
   const classes = useStyles();
   const [loading, setLoading] = React.useState(true);
   const [chats, setChats] = React.useState<Array<any>>(chatData.chats);
+  const [message, setMessage] = React.useState<string>("");
+  const lastMessageRef = React.useRef<any>(null);
+  React.useEffect(()=> {
+    if(lastMessageRef.current){
+      lastMessageRef.current.scrollIntoView({behaviour: 'smooth'})
+    }
+  },[chats])
+  const sendMessage = () => {
+    const newMessage = {
+      message,
+      direction: "outgoing",
+      sentDate: new Date(),
+      readStatus: false,
+    };
+    const newChats = [...chats, newMessage];
+    setChats(newChats);
+  };
   return (
     <div className={classes.container}>
       {loading && <Spinner animation="border" variant="success" />}
       <div className={classes.header}>{chatData.senderName}</div>
       <div className={classes.chatsBox}>
-        {chats.map((item) => {
-          return <Chats {...item} />;
+        {chats.map((item, index) => {
+          return (
+            <div ref={index === chats.length - 1 ? lastMessageRef : undefined}>
+              <Chats {...item} />
+            </div>
+          );
         })}
       </div>
-      <div className={classes.messageBox}>messagebox</div>
+      <div className={classes.messageBox}>
+        messagebox
+        <div>
+          <input
+            type="text"
+            alt="type message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
 };
